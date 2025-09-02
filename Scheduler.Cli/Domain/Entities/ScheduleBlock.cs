a8e9d2ab2f7035bd2164b541a7b694b29560def9
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-02 16:54:46 +0200

    Add Studio.cs for handling into SchedulDay class

--- Scheduler.Cli/Domain/Entities/ScheduleBlock.cs
+++ Scheduler.Cli/Domain/Entities/ScheduleBlock.cs
@@ -6,16 +6,20 @@ namespace Scheduler.Cli.Domain.Entities
     public class ScheduleBlock
     {
         public TimeOfDayRange Range { get; }
-        public object Content { get; }
-        
-        public ScheduleBlock(TimeOfDayRange range, object content) 
-        {
-            if(content == null) throw new ArgumentNullException("Content is null");
+        public string Title { get; }
 
+        public Studio Studio { get; }
+
+        public ScheduleBlock(TimeOfDayRange range, string title, Studio studio)
+        {
             if(range == null) throw new ArgumentNullException(nameof(range));
+            if(string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title cannot be empty.", nameof(title));
+            if (studio == Studio.Unknown) throw new ArgumentException("Studio cannot be Unknown", nameof(studio));
 
             Range = range;
-            Content = content;
+            Title = title;
+            Studio = studio;
+            
         }
 
     }
---
+++ Scheduler.Cli/Domain/ValueObjects/Studio.cs
@@ -0,0 +1,12 @@
+﻿namespace Scheduler.Cli.Domain.ValueObjects;
+
+
+
+public enum Studio
+{
+    Unknown = 0,
+    Studio1 = 1,
+    Studio2 = 2,
+    Remote = 3
+}
+
