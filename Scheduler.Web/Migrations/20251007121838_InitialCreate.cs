a562ea92f47dd71c951db4786b1642c4b019dfff
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-10-07 14:20:53 +0200

    Finally created the Initial migrations

---
+++ Scheduler.Web/Migrations/20251007121838_InitialCreate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scheduler.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleDays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleDays", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleBlocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Range_Start = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    Range_End = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Studio = table.Column<int>(type: "INTEGER", nullable: false),
+                    ScheduleDayId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleBlocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleBlocks_ScheduleDays_ScheduleDayId",
+                        column: x => x.ScheduleDayId,
+                        principalTable: "ScheduleDays",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ScheduleBlockId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Guests_ScheduleBlocks_ScheduleBlockId",
+                        column: x => x.ScheduleBlockId,
+                        principalTable: "ScheduleBlocks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Presenters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ScheduleBlockId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Presenters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Presenters_ScheduleBlocks_ScheduleBlockId",
+                        column: x => x.ScheduleBlockId,
+                        principalTable: "ScheduleBlocks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_ScheduleBlockId",
+                table: "Guests",
+                column: "ScheduleBlockId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Presenters_ScheduleBlockId",
+                table: "Presenters",
+                column: "ScheduleBlockId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleBlocks_ScheduleDayId",
+                table: "ScheduleBlocks",
+                column: "ScheduleDayId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guests");
+
+            migrationBuilder.DropTable(
+                name: "Presenters");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleBlocks");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleDays");
+        }
+    }
+}
