bfdd7a8b14e24b8eb1828061a897b4a207af567a
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-10-07 13:17:28 +0200

    Add a /Data/SchedulerDbContext

---
+++ Scheduler.Web/Data/SchedulerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scheduler.Domain.Entities;
+
+namespace Scheduler.Web.Data
+{
+    public class SchedulerDbContext : DbContext
+    {
+        public SchedulerDbContext(DbContextOptions<SchedulerDbContext> options)
+            : base(options)
+        {
+        }
+
+        // 🔹 Tabeller
+        public DbSet<ScheduleDay> ScheduleDays => Set<ScheduleDay>();
+        public DbSet<ScheduleBlock> ScheduleBlocks => Set<ScheduleBlock>();
+        public DbSet<Presenter> Presenters => Set<Presenter>();
+        public DbSet<Guest> Guests => Set<Guest>();
+
+    }
+}
+
