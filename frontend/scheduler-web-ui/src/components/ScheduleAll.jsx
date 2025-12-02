c194b45de89c2e6f5e7b408a05a06c894723cc3c
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-12-02 10:07:45 +0100

    Add a pagination for the SchedulerAll

--- frontend/scheduler-web-ui/src/components/ScheduleAll.jsx
+++ frontend/scheduler-web-ui/src/components/ScheduleAll.jsx
@@ -15,115 +15,160 @@ function ScheduleAll() {
     const [error, setError] = useState([]);
     const [show, setShow] = useState(false);
     const [selectedId, setSelectedId] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         getAll()
             .then(data => {
-                const today = new Date();
-                today.setHours(0, 0, 0, 0); // nollställ tid eftersom dagen klocka tickar
+                const sorted = data.sort((a, b) =>
+                    new Date(a.date) - new Date(b.date)
+                );
 
-                const filtered = data.filter(day => {
-                    const d = new Date(day.date + "T00:00:00");
-                    return d >= today;
+                sorted.forEach(day => {
+                    day.blocks.sort((a, b) => a.range.start.localeCompare(b.range.start));
                 });
 
-                filtered.forEach(day => {
-                    day.blocks.sort((a, b) => {
-                        const t1 = a.range.start;
-                        const t2 = b.range.start;
-                        return t1.localeCompare(t2);
-                    });
-                });
+                setSchedule(sorted);
 
-                setSchedule(filtered);
+                const todayStr = new Date().toISOString().split("T")[0];
+                const index = sorted.findIndex(d => d.date === todayStr);
+
+
+                setCurrentIndex(index >= 0 ? index : 0);
+
             })
             .catch(err => setError(err.message));
-    }, []);
+}, []);
 
-    const handleOpen = (id) => {
-        setSelectedId(id);
-        setShow(true);
-    };
+const handleOpen = (id) => {
+    setSelectedId(id);
+    setShow(true);
+};
 
-    const refreshData = async () => {
-        try {
-            const data = await getAll();
-            setSchedule(data);
-        } catch (err) {
-            console.error("Kunde inte uppdatera listan:", err);
-        }
-    };
+const refreshData = async () => {
+    try {
+        const data = await getAll();
+        setSchedule(data);
+    } catch (err) {
+        console.error("Kunde inte uppdatera listan:", err);
+    }
+};
+
+const handleClose = () => setShow(false);
+
+const prev = () => {
+    if (currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+    }
+};
+
+const next = () => {
+    if (currentIndex < schedule.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+    }
+};
+
+const currentDay = schedule[currentIndex];
+
+return (
+    <>
+        {typeof error === "string" && error.trim() !== "" && (
+
+            <Row>
+                <Col className="col-12">
+                    <Alert variant="danger" dismissible>
+                        <Alert.Heading>Fel:</Alert.Heading>
+                        <p>{error}</p>
+                    </Alert>
+                </Col>
+            </Row>
+
+        )}
+
+        <Row>
+            <Col className="col-12">
+                {/*<h1>Schemaöversikt</h1>*/}
+            </Col>
+
+            {!error && schedule.length === 0 && <p>Laddar eller tomt schema...</p>}
+        </Row>
 
 
-    const handleClose = () => setShow(false);
+        <Row className="mt-2">
+            <Col className="col-12">
+                <ListGroup>
+                    {schedule.length > 0 && currentDay?.blocks.length > 0 ? (
 
-    return (
-        <>
-            {typeof error === "string" && error.trim() !== "" && (
-            
-                <Row>
-                    <Col className="col-12">
-                        <Alert variant="danger" dismissible>
-                            <Alert.Heading>Fel:</Alert.Heading>
-                            <p>{error}</p>
-                        </Alert>
-                    </Col>
-                </Row>
-            
-            )}
-            
-                <Row>
-                    <Col className="col-12">
-                        {/*<h1>Schemaöversikt</h1>*/}
-                    </Col>
+                        currentDay.blocks.map(block =>
+                        (
+                            <ListGroup.Item key={block.id} className="list-group-item-action">
 
-                    {!error && schedule.length === 0 && <p>Laddar eller tomt schema...</p>}
-                </Row>
-            
-            
-                <Row className="mt-2">
-                    <Col className="col-12">
-                        <ListGroup>
-                            {schedule.map(day => (
+                                <div>
+                                    <small>Datum: {currentDay.date} : Block ID:{block.id}</small>
+                                    <div>
+                                        {block.range.start.slice(0, 5)} - {block.range.end.slice(0, 5)}
+                                        <span className="fw-bold"> - {block.title}</span> - Längd: {block.range.duration.slice(0, 5)}
+                                    </div>
+                                </div>
 
-                                day.blocks.map(block =>
-                                (
-                                    <ListGroup.Item key={block.id} className="list-group-item-action">
+                                <div className="d-flex align-items-center">
+                                    {block.presenters?.length > 0 && (
                                         <div>
-                                            <small>Datum: {day.date} : Block ID:{block.id}</small>
-                                            <div>
-                                                {block.range.start.slice(0, 5)} - {block.range.end.slice(0, 5)} <span className="fw-bold"> - {block.title}</span> - Längd: {block.range.duration.slice(0, 5)}
-                                            </div>
+                                            med <span className="fw-bold">{block.presenters.map(p => p.name).join(", ")} </span>
+                                            <span>
+                                                i Studio {block.studio}
+                                            </span>
                                         </div>
-                                        <div className="d-flex align-items-center">
-                                            {block.presenters?.length > 0 && (
-                                                <div>
-                                                    med <span className="fw-bold">{block.presenters.map(p => p.name).join(", ")} </span>
-                                                    <span>
-                                                        i Studio {block.studio}
-                                                    </span>
-                                                </div>
-                                            )}
-                                            {block.guests?.length > 0 && (
-                                                <span className="mx-2"> Gäst/Gäster: {block.guests.map(g => g.name).join(", ")}</span>
-                                            )}
-                                            <Button className="btn btn-primary ms-auto" onClick={() => handleOpen(block.id)}><FaMagnifyingGlass/></Button>
-                                        </div>
-                                    </ListGroup.Item>
-                                )
-                                )))
-                            }
-                        </ListGroup>
-                    </Col>
-                </Row>
-            <BsModal
-                show={show}
-                onHide={() => setShow(false)}
-                id={selectedId}
-                onDeleted={refreshData}
-            />
-        </>
-    )
+                                    )}
+
+                                    {block.guests?.length > 0 && (
+                                        <span className="mx-2"> Gäst/Gäster: {block.guests.map(g => g.name).join(", ")}</span>
+                                    )}
+
+                                    <Button className="btn btn-primary ms-auto" onClick={() => handleOpen(block.id)}>
+                                        <FaMagnifyingGlass />
+                                    </Button>
+                                </div>
+                            </ListGroup.Item>
+                        )
+                        )) : (
+                    <ListGroup.Item>
+                        <em> Inga block denna dag.</em>
+                    </ListGroup.Item>
+                    )
+                    }
+                </ListGroup>
+                <div className="d-flex justify-content-between mt-3">
+                    <Button
+                        variant="secondary"
+                        disabled={currentIndex === 0}
+                        onClick={prev}
+                    >
+                        Föregående dag
+                    </Button>
+
+                    <div>
+                        {currentDay?.date}
+                    </div>
+
+                    <Button
+                        variant="secondary"
+                        disabled={currentIndex === schedule.length - 1}
+                        onClick={next}
+                    >
+                        Nästa dag
+                    </Button>
+                </div>
+            </Col>
+        </Row>
+        <BsModal
+            show={show}
+            onHide={() => setShow(false)}
+            id={selectedId}
+            onDeleted={refreshData}
+        />
+    </>
+)
 }
 
 export default ScheduleAll
